6eeffa0621f29ae9d4792fba7302bbdd58e425b1
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-12-03 22:07:53 +0700

    telah menyelesaikan materi ke 27

--- quiz-vue/src/App.vue
+++ quiz-vue/src/App.vue
@@ -1,15 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import srcQuiz from "./data/quizes.json";
 
 const quizes = ref(srcQuiz);
+const search = ref("");
+
+/* 
+ini awal nya agak triki sih, cukup bagi saya ulik fungsi filter dan includes saya jadi faham Alhamdulillah.
+
+ini hasil latihan saya:
+let nama = ["icam","Fadilah","Futuh","iqram"];
+let cari = "fadilah";
+let hasil = nama.filter(nam => { 
+    return nam.toLowerCase().includes(cari.toLowerCase());
+});
+*/
+watch(search, () => {
+  quizes.value = srcQuiz.filter((quiz) => {
+    return quiz.title.toLowerCase().includes(search.value.toLowerCase());
+  });
+});
 </script>
 
 <template>
   <main>
     <header>
       <h1 id="title">Quiz Vue</h1>
-      <input type="text" id="search-input" />
+      <input v-model.trim="search" type="text" id="search-input" />
     </header>
     <section id="quize-container">
       <div v-for="quize in quizes" :key="quize.id" class="card">
